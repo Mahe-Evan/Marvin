732396fe2322ca416cc5f4a348a4adfde9a86e93
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-03-07 10:36:49 +0100

    feat: add `clone-repository` job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -3,8 +3,35 @@ folder("Tools") {
     description("Folder for miscellaneous tools.")
 }
 
-job("Tools/test") {
+job("Tools/clone-repository") {
+    parameters {
+        stringParam {
+            name("GIT_REPOSITORY_URL")
+            description("Git URL of the repository to clone")
+        }
+    }
+
+    wrappers {
+        preBuildCleanup {
+            includePattern('.git')
+            includePattern('.gitmodules')
+            includePattern('target/')
+            includePattern('dist/')
+            includePattern('build/')
+            includePattern('out/')
+            includePattern('node_modules/')
+            includePattern('venv/')
+            includePattern('__pycache__/')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+
     steps {
-        shell("echo 'Hello, World!'")
+        shell('git clone $GIT_REPOSITORY_URL')
+    }
+
+    publishers {
+        wsCleanup()
     }
 }
