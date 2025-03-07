3b32a0f12e56505b637c6c361892d0bdbb200d03
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-03-07 14:12:38 +0100

    feat: add SEED job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -35,3 +35,56 @@ job("Tools/clone-repository") {
         wsCleanup()
     }
 }
+
+job ("Tools/SEED") {
+    parameters {
+        stringParam {
+            name("GITHUB_NAME")
+            description("GitHub repository owner/repo_name (e.g.: \"EpitechIT31000/chocolatine\")")
+        }
+
+        stringParam {
+            name("DISPLAY_NAME")
+            description("Display name for the job")
+        }
+    }
+
+    steps {
+        dsl {
+            text('''
+                def githubName = binding.variables.GITHUB_NAME
+                def displayName = binding.variables.DISPLAY_NAME
+
+                job(displayName) {
+                    properties {
+                        githubProjectUrl("https://github.com/${githubName}")
+                    }
+
+                    triggers {
+                        scm('* * * * *')
+                    }
+
+                    scm {
+                        git {
+                            remote {
+                                github(githubName)
+                            }
+                            branches('*/main', '*/master')
+                        }
+                    }
+
+                    wrappers {
+                        preBuildCleanup()
+                    }
+
+                    steps {
+                        shell('make fclean')
+                        shell('make')
+                        shell('make tests_run')
+                        shell('make clean')
+                    }
+                }
+            ''')
+        }
+    }
+}
