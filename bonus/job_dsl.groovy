3839e6517f511594d969a18302cee578ab0dffcc
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-03-21 14:57:40 +0100

    feat(bonus): update Docker setup and add new job DSL
    - added envinject plugin to Dockerfile
    - changed configuration file mapping in docker-compose
    - created a new YAML config for Jenkins with user roles
    - updated job DSL script to define a new pipeline job with parameters
    - added network configuration for services in docker-compose

--- bonus/job_dsl.groovy
+++ bonus/job_dsl.groovy
@@ -36,41 +36,132 @@ job("Tools/clone-repository") {
     }
 }
 
-// Create a pipeline job that uses the pipeline definition
-pipelineJob("EpitestPipeline") {
+job ("Tools/SEED") {
     parameters {
         stringParam {
             name("GITHUB_NAME")
             description("GitHub repository owner/repo_name (e.g.: \"EpitechIT31000/chocolatine\")")
         }
+
         stringParam {
             name("DISPLAY_NAME")
             description("Display name for the job")
         }
-    }
-    
-    properties {
-        githubProjectProperty {
-            projectUrlStr('${env.GITHUB_USERNAME}')
+
+        stringParam {
+            name("EXECUTABLE")
+            description("Name of the executable to run")
         }
     }
-    
-    triggers {
-        scm('* * * * *')
-    }
-    
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        github('${env.GITHUB_NAME}')
-                        credentials('github-credentials')
+
+    steps {
+        dsl {
+            text('''
+                def githubName = binding.variables.GITHUB_NAME
+                def displayName = binding.variables.DISPLAY_NAME
+                def executable = binding.variables.EXECUTABLE
+
+                job(displayName) {
+                    properties {
+                        githubProjectUrl("https://github.com/${githubName}")
                     }
-                    branches('*/main', '*/master')
+
+                    triggers {
+                        scm('* * * * *')
+                    }
+
+                    scm {
+                        git {
+                            remote {
+                                github(githubName)
+                                credentials('github-credentials')
+                            }
+                            branches('*/main', '*/master')
+                        }
+                    }
+
+                    wrappers {
+                        preBuildCleanup()
+                    }
+
+                    environmentVariables {
+                        env('WORKSPACE', "/mnt/jenkins_builds/${'$'}{JOB_NAME}_${'$'}{BUILD_ID}")
+                        env('EXECUTABLE', executable)
+                    }
+
+                    steps {
+                        shell("""
+                            # Set up the workspace
+                            : "############################################################"
+                            : "#                   Setting up Workspace                   #"
+                            : "############################################################"
+                            mkdir -p /mnt/jenkins_builds
+                            mkdir -p ${'$'}WORKSPACE
+                            cp -r * ${'$'}WORKSPACE
+                        """.stripIndent())
+
+                        shell("""
+                            # Compile the project
+                            : "###########################################################"
+                            : "#                    Compiling Project                    #"
+                            : "###########################################################"
+
+                            cd ${'$'}WORKSPACE
+
+                            # Run the Docker container
+                            docker exec mymarvin-epitest-1 bash -c "cd ${'$'}WORKSPACE && (make clean) > output.txt"
+                            cat output.txt
+                            docker exec mymarvin-epitest-1 bash -c "cd ${'$'}WORKSPACE && (make) > output.txt"
+                            cat output.txt
+
+                            # Check if the executable exists
+                            if [ ! -f "${'$'}WORKSPACE/${'$'}EXECUTABLE" ]; then
+                                : "❌ Executable not found! Exiting with status 1."
+                                exit 1
+                            else
+                                : "✅ Executable found: ${'$'}EXECUTABLE"
+                            fi
+
+                            # Clean up the workspace
+                            docker exec mymarvin-epitest-1 bash -c "cd ${'$'}WORKSPACE && (make fclean) > output.txt"
+                            cat output.txt
+                        """.stripIndent())
+
+                        shell("""
+                            # Check the coding style
+                            : "###########################################################"
+                            : "#                  Checking Coding Style                  #"
+                            : "###########################################################"
+
+                            cd ${'$'}WORKSPACE
+
+                            # Run the coding style checker
+                            docker run --rm --security-opt "label:disable" -i \
+                            -v "${'$'}WORKSPACE":"/mnt/delivery" \
+                            -v "${'$'}WORKSPACE":"/mnt/reports" \
+                            ghcr.io/epitech/coding-style-checker:latest "/mnt/delivery" "/mnt/reports"
+
+                            # Check if the report file exists
+                            if [ -s "coding-style-reports.log" ]; then
+                                cat coding-style-reports.log
+                                : "❌ Coding style errors detected! Exiting with status 1."
+                                exit 1
+                            else
+                                : "✅ No coding style issues found."
+                            fi
+
+                        """.stripIndent())
+
+                        shell("""
+                            # Clean up the workspace
+                            : "###########################################################"
+                            : "#                  Cleaning up Workspace                  #"
+                            : "###########################################################"
+                            rm -rf ${'$'}WORKSPACE
+                        """.stripIndent())
+                    }
                 }
-            }
-            scriptPath('Jenkinsfile')
+            ''')
         }
     }
 }
